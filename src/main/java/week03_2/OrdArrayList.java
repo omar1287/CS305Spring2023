fac1bd9853b6dca027cfd4cfe94febf97a5aa602
Moody <asdas201888@gmail.com>
2022-11-03 13:16:52 +0200

    add OrdArrayList missed methods

--- src/main/java/week03_2/OrdArrayList.java
+++ src/main/java/week03_2/OrdArrayList.java
@@ -8,132 +8,138 @@ package week03_2;/*
  */
 public class OrdArrayList {
 
-  private double[] a;                 // ref to array a
-  private int n;               // number of data items
-  //-----------------------------------------------------------
+    private double[] a;                 // ref to array a
+    private int n;               // number of data items
+    //-----------------------------------------------------------
 
-  public OrdArrayList(int max) // constructor
-  {
-    a = new double[max];                 // create the array
-    n = 0;                        // no items yet
-  }
-
-  public void clear() {
-    n = 0;
-  }
-
-  public double getFirst() {
-    if (n == 0) {
-      System.out.println("Empty List");
-      return -1;
+    public OrdArrayList(int max) // constructor
+    {
+        a = new double[max];                 // create the array
+        n = 0;                        // no items yet
     }
-    return a[0];
-  }
 
-  public double getLast() {
-    if (n == 0) {
-      System.out.println("Empty List");
-      return -1;
+    public void clear() {
+        n = 0;
     }
-    return a[n - 1];
-  }
 
-  //----------------------- Binary Search O(log n) ------------------------
-  public int find(double searchKey) {                              // find specified value
-    int lowerBound = 0;
-    int upperBound = n - 1;
-    int curIn;
-
-    while (true) {
-      curIn = (lowerBound + upperBound) / 2;
-      if (a[curIn] == searchKey) {
-        return curIn;              // found it
-      } else if (lowerBound > upperBound) {
-        return -1;             // can't find it
-      } else // divide range
-      {
-        if (a[curIn] < searchKey) {
-          lowerBound = curIn + 1; // it's in upper half
-        } else {
-          upperBound = curIn - 1; // it's in lower half
+    public double getFirst() {
+        if (n == 0) {
+            System.out.println("Empty List");
+            return -1;
         }
-      }  // end else divide range
-    }  // end while
-  }  // end find()
-  //----------------------- Insert Key in Order O(n) ---------------------
-
-  public void insert(double value) // put element into array
-  {
-    int j = 0;
-    for (j = 0; j < n; j++) // find where it goes
-    {
-      if (a[j] > value) // (linear search)
-      {
-        break;
-      }
+        return a[0];
     }
-    for (int k = n; k > j; k--) // move bigger ones up
-    {
-      a[k] = a[k - 1];
+
+    public double getLast() {
+        if (n == 0) {
+            System.out.println("Empty List");
+            return -1;
+        }
+        return a[n - 1];
     }
-    a[j] = value;                  // insert it
-    n++;                      // increment size
-  }
-  //----------------------- Delete Key O(n) ----------------------------
 
-  public boolean delete(double value) {
-    int j = find(value);
+    //----------------------- Binary Search O(log n) ------------------------
+    public int find(double searchKey) {                              // find specified value
+        int lowerBound = 0;
+        int upperBound = n - 1;
+        int curIn;
 
-    if (j == -1) // can't find it
+        while (true) {
+            curIn = (lowerBound + upperBound) / 2;
+            if (a[curIn] == searchKey) {
+                return curIn;              // found it
+            } else if (lowerBound > upperBound) {
+                return -1;             // can't find it
+            } else // divide range
+            {
+                if (a[curIn] < searchKey) {
+                    lowerBound = curIn + 1; // it's in upper half
+                } else {
+                    upperBound = curIn - 1; // it's in lower half
+                }
+            }  // end else divide range
+        }  // end while
+    }  // end find()
+    //----------------------- Insert Key in Order O(n) ---------------------
+
+    public void insert(double value) // put element into array
     {
-      return false;
-    } else // found it
-    {
-      for (int k = j; k < n; k++) // move higher ones down
-      {
-        a[k] = a[k + 1];
-      }
-      n--;                   // decrement size
-      return true;
+        int j = 0;
+        for (j = 0; j < n; j++) // find where it goes
+        {
+            if (a[j] > value) // (linear search)
+            {
+                break;
+            }
+        }
+        for (int k = n; k > j; k--) // move bigger ones up
+        {
+            a[k] = a[k - 1];
+        }
+        a[j] = value;                  // insert it
+        n++;                      // increment size
     }
-  }  // end delete()
+    //----------------------- Delete Key O(n) ----------------------------
 
-  //------------------------ Find Min O(1) ------------------------------
-  public double findMin() {
-    // Your code here
-    return 0;
-  }
+    public boolean delete(double value) {
+        int j = find(value);
 
-  //------------------------ Find Max O(1) ------------------------------
-  public double findMax() {
-    // Your code here
-    return 0;
-  }
+        if (j == -1) // can't find it
+        {
+            return false;
+        } else // found it
+        {
+            for (int k = j; k < n; k++) // move higher ones down
+            {
+                a[k] = a[k + 1];
+            }
+            n--;                   // decrement size
+            return true;
+        }
+    }  // end delete()
 
-  //------------------------ Find Next O(log n) ------------------------------
-  // return the stored item with smallest key larger than k
-  public double findNext(double k) {
-    int j = find(k);
-    if (j == -1 || j == n - 1) {
-      return -1;
+    //------------------------ Find Min O(1) ------------------------------
+    public double findMin() {
+        // Your code here
+        if (n == 0) return -1;
+        return a[0];
     }
-    return a[j + 1];
-  }
 
-  //------------------------ Find Prev O(log n) ------------------------------
-  // return the stored item with largest key smaller than k
-  public double findPrev(double k) {
-    // Your code here
-    return 0;
-  }
+    //------------------------ Find Max O(1) ------------------------------
+    public double findMax() {
+        // Your code here
+        if (n == 0) return -1;
+        return a[n - 1];
+    }
+
+    //------------------------ Find Next O(log n) ------------------------------
+    // return the stored item with the smallest key larger than k
+    public double findNext(double k) {
+        int j = find(k);
+        if (j == -1 || j == n - 1) {
+            return -1;
+        }
+        return a[j + 1];
+    }
+
+    //------------------------ Find Prev O(log n) ------------------------------
+    // return the stored item with the largest key smaller than k
+    public double findPrev(double k) {
+        // Your code here
+        int j = find(k);
+        if (j == -1 || j == 0) {
+            return -1;
+        }
+        return a[j - 1];
+    }
 //------------------------ Display Elements O(n) ----------------------
 
-  public void display() // displays array contents
-  {
-    for (int j = 0; j < n; j++) // for each element,
+    public void display() // displays array contents
     {
-      System.out.print(a[j] + " ");  // display it
+        for (int j = 0; j < n; j++) // for each element,
+        {
+            System.out.print(a[j] + " ");  // display it
+        }
+        System.out.println("");
     }
-    System.out.println("");
-  }
 }
